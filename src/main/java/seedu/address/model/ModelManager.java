a7ed76e68119cfd7d37786f9cde3b6beb73832b7
ZiyangMou <hflsmzy@gmail.com>
2017-03-11 11:47:10 +0800

    Add list level method in ModelManager

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -14,6 +14,10 @@ import seedu.address.model.task.Task;
 import seedu.address.model.task.ReadOnlyTask;
 import seedu.address.model.task.UniqueTaskList;
 import seedu.address.model.task.UniqueTaskList.TaskNotFoundException;
+import seedu.address.model.tasklist.TaskList;
+import seedu.address.model.tasklist.UniqueListList;
+import seedu.address.model.tasklist.UniqueListList.DuplicateListException;
+import seedu.address.model.tasklist.UniqueListList.ListNotFoundException;
 
 /**
  * Represents the in-memory model of the address book data.
@@ -24,6 +28,7 @@ public class ModelManager extends ComponentManager implements Model {
 
     private final TaskManager taskManager;
     private final FilteredList<ReadOnlyTask> filteredTasks;
+    private final FilteredList<TaskList> filteredLists;
 
     /**
      * Initializes a ModelManager with the given taskManager and userPrefs.
@@ -36,6 +41,7 @@ public class ModelManager extends ComponentManager implements Model {
 
         this.taskManager = new TaskManager(taskManager);
         filteredTasks = new FilteredList<>(this.taskManager.getTaskList());
+        filteredLists = new FilteredList<>(this.taskManager.getListList());
     }
 
     public ModelManager() {
@@ -67,7 +73,7 @@ public class ModelManager extends ComponentManager implements Model {
     @Override
     public synchronized void addTask(Task task) throws UniqueTaskList.DuplicateTaskException {
         taskManager.addTask(task);
-        updateFilteredListToShowAll();
+        updateFilteredListToShowAllTasks();
         indicateTaskManagerChanged();
     }
 
@@ -80,6 +86,29 @@ public class ModelManager extends ComponentManager implements Model {
         taskManager.updateTask(taskManagerIndex, editedTask);
         indicateTaskManagerChanged();
     }
+    
+    @Override
+    public synchronized void addList(TaskList list) throws UniqueListList.DuplicateListException {
+        assert list != null;
+        taskManager.addList(list);
+        updateFilteredListToShowAllLists();
+        indicateTaskManagerChanged();
+    }
+    
+    @Override
+    public synchronized void removeList(TaskList target) throws ListNotFoundException {
+        taskManager.removeList(target);
+        indicateTaskManagerChanged();
+    }
+    
+    @Override
+    public void updateList(int filteredListListIndex, TaskList editedList) throws DuplicateListException {
+        assert editedList != null;
+
+        int taskManagerIndex = filteredLists.getSourceIndex(filteredListListIndex);
+        taskManager.updateList(taskManagerIndex, editedList);
+        indicateTaskManagerChanged();
+    }
 
     //=========== Filtered Task List Accessors =============================================================
 
@@ -89,7 +118,7 @@ public class ModelManager extends ComponentManager implements Model {
     }
 
     @Override
-    public void updateFilteredListToShowAll() {
+    public void updateFilteredListToShowAllTasks() {
         filteredTasks.setPredicate(null);
     }
 
@@ -102,10 +131,31 @@ public class ModelManager extends ComponentManager implements Model {
         filteredTasks.setPredicate(expression::satisfies);
     }
 
+    //=========== Filtered List List Accessors ==============================================================
+
+    @Override
+    public UnmodifiableObservableList<TaskList> getFilteredListList() {
+        return new UnmodifiableObservableList<>(filteredLists);
+    }
+    
+    @Override
+    public void updateFilteredListToShowAllLists() {
+        filteredLists.setPredicate(null);
+    }
+
+    @Override
+    public void updateFilteredListList(Set<String> keywords) {
+        updateFilteredListList(new PredicateExpression(new NameQualifier(keywords)));
+    }
+
+    private void updateFilteredListList(Expression expression) {
+        filteredLists.setPredicate(expression::satisfies);
+    }
     //========== Inner classes/interfaces used for filtering =================================================
 
     interface Expression {
         boolean satisfies(ReadOnlyTask task);
+        boolean satisfies(TaskList list);
         String toString();
     }
 
@@ -123,6 +173,11 @@ public class ModelManager extends ComponentManager implements Model {
         }
 
         @Override
+        public boolean satisfies(TaskList list){
+            return qualifier.run(list);
+        }
+        
+        @Override
         public String toString() {
             return qualifier.toString();
         }
@@ -130,6 +185,7 @@ public class ModelManager extends ComponentManager implements Model {
 
     interface Qualifier {
         boolean run(ReadOnlyTask task);
+        boolean run(TaskList list);
         String toString();
     }
 
@@ -144,6 +200,14 @@ public class ModelManager extends ComponentManager implements Model {
         public boolean run(ReadOnlyTask task) {
             return nameKeyWords.stream()
                     .filter(keyword -> StringUtil.containsWordIgnoreCase(task.getName().fullName, keyword))
+                    .findAny()
+                    .isPresent();
+        }
+        
+        @Override
+        public boolean run(TaskList list) {
+            return nameKeyWords.stream()
+                    .filter(keyword -> StringUtil.containsWordIgnoreCase(list.getName(), keyword))
                     .findAny()
                     .isPresent();
         }
