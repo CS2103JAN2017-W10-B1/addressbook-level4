67a19aa70f5688f3e4fd422309fe2646206da14d
Shermine Jong <shermine_jong_ruth_93@outlook.sg>
2017-03-13 15:24:24 +0800

    [V0.2][Logic] add an finish command

--- src/main/java/seedu/address/logic/commands/FinishCommand.java
+++ src/main/java/seedu/address/logic/commands/FinishCommand.java
@@ -1,5 +1,58 @@
 package seedu.address.logic.commands;
 
-public class FinishCommand {
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.UnmodifiableObservableList;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.task.ReadOnlyTask;
+import seedu.address.model.task.UniqueTaskList.TaskNotFoundException;
+
+
+/**
+ * Deletes a person identified using it's last displayed index from the address book.
+ */
+public class FinishCommand extends Command {
+
+    public static final String COMMAND_WORD = "finish";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Marks the task identified by the index number used in the last task listing as finished.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_FINISH_TASK_SUCCESS = "Mark finished task: %1$s";
+    public static final String MESSAGE_FINISH_TASK_MARKED = "Task had been finished: %1$s";
+
+    public final int targetIndex;
+
+    public FinishCommand(int targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+
+    @Override
+    public CommandResult execute() throws CommandException {
+
+        UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
+
+        if (lastShownList.size() < targetIndex) {
+            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        }
+
+        ReadOnlyTask taskToMark = lastShownList.get(targetIndex - 1);
+
+        try {
+        	if(taskToMark.isFinished()){
+        		throw new CommandException(MESSAGE_FINISH_TASK_MARKED) ;
+        	}
+        	else{
+        		taskToMark.
+        	}
+        } catch (TaskNotFoundException pnfe) {
+            assert false : "The target person cannot be missing";
+        }
+
+        return new CommandResult(String.format(MESSAGE_FINISH_TASK_SUCCESS, personToDelete));
+    }
 
 }
+
