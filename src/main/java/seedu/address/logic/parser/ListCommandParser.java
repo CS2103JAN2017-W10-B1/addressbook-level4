fc8c942d83a97026744a50a2681838ec79abd907
ZiyangMou <hflsmzy@gmail.com>
2017-03-15 00:30:07 +0800

    [List Command] Add List Command Parser

---
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -0,0 +1,39 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.parser.CliSyntax.KEYWORDS_ARGS_FORMAT_LIST;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.ListCommand;
+
+/**
+ * Parses input arguments and creates a new ListCommand object
+ */
+public class ListCommandParser {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns an FindCommand object for execution.
+     */
+    public Command parse(String args) {
+        if (args == null) {
+            return new ListCommand();
+        }
+        
+        // if not match, show all the unfinished task
+        final Matcher matcher = KEYWORDS_ARGS_FORMAT_LIST.matcher(args.trim());
+        if (!matcher.matches()) {
+            return new ListCommand();
+        }
+
+        // keywords delimited by whitespace
+        final String[] keywords = matcher.group("keywords").split("\\s+");
+        final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
+        return new ListCommand(keywordSet);
+    }
+
+}
