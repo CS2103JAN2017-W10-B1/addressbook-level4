bf9597e1f77efeefccbb5793a6968e06c99edf94
ZiyangMou <hflsmzy@gmail.com>
2017-03-15 00:30:40 +0800

    [Test] Test ListCommandParser

---
+++ src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
@@ -0,0 +1,61 @@
+package seedu.address.logic.parser;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.logic.commands.ListCommand;
+
+public class ListCommandParserTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    
+    @Test
+    public void nullTest() throws Exception{
+
+        Field field = ListCommand.class.getDeclaredField("keywords");
+        field.setAccessible(true);
+        
+        ListCommandParser tester = new ListCommandParser();
+        ListCommand listCommand =  (ListCommand) tester.parse(null);
+        assertEquals(field.get(listCommand), null);
+    }
+    
+    @Test
+    public void singleFieldTest() throws Exception {
+        
+        Field field = ListCommand.class.getDeclaredField("keywords");
+        field.setAccessible(true);
+        
+        ListCommandParser tester = new ListCommandParser();
+        ListCommand listCommand =  (ListCommand) tester.parse("personal");
+        
+        Set<String> set = new HashSet<String>();
+        set.add("personal");
+        
+        assertEquals(field.get(listCommand), set);
+    }
+    
+    /*@Test
+    public void nultipleieldTest() throws Exception {
+        
+        Field field = ListCommand.class.getDeclaredField("keywords");
+        field.setAccessible(true);
+        
+        ListCommandParser tester = new ListCommandParser();
+        ListCommand listCommand =  (ListCommand) tester.parse("personal school");
+        
+        Set<String> set = new HashSet<String>();
+        set.add("personal");
+        set.add("school");
+        
+        assertEquals(field.get(listCommand), set);
+    }*/
+}
