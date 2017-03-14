c2e7b7d231c9597ae78a545c48a15ebee8d44af7
ZiyangMou <hflsmzy@gmail.com>
2017-03-15 00:29:47 +0800

    [List Command] Allow list command accept parameters to find tasks in list

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Lists all persons in the address book to the user.
@@ -10,14 +12,42 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all tasks";
     
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": List tasks as per the parameters"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": List tasks as per the parameters\n"
             + "the specified listname/list index and displays them as a list with index numbers.\n"
             + "Parameters: [LISTNAME/LISTINDEX]\n"
             + "Example: " + COMMAND_WORD + " CS2103";
+    
+    private static final String LIST_SEPARATOR = ", ";
+    
+    private final Set<String> keywords;
+
+    public ListCommand(Set<String> keywords) {
+        this.keywords = keywords;
+    }
+    
+    public ListCommand() {
+        this.keywords = null;
+    }
 
     @Override
     public CommandResult execute() {
-        model.updateFilteredListToShowAllTasks();
-        return new CommandResult(MESSAGE_SUCCESS);
+        if (keywords == null) {
+            model.updateFilteredListToShowAllTasks();
+            return new CommandResult(MESSAGE_SUCCESS);
+        } else {
+            model.updateFilteredTaskListGivenListName(keywords);
+            return new CommandResult(formatter(keywords));
+        }    
+    }
+    
+    private String formatter(Set<String> keywords) {
+        String formatted = MESSAGE_SUCCESS + " in list ";
+        for (Iterator<String> it = keywords.iterator(); it.hasNext(); ) {
+            formatted += it.next();
+            if (it.hasNext()) {
+                formatted += LIST_SEPARATOR;
+            }
+        }
+        return formatted;
     }
 }
