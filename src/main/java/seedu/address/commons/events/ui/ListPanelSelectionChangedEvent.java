a40d6cf6e375ad8fcaeacb82da9b5acefce8d9d2
Yan Xiaoxuan <E0012668@u.nus.edu>
2017-03-14 19:43:27 +0800

    [V0.2][Commons] Add changes in list panel event handling class

---
+++ src/main/java/seedu/address/commons/events/ui/ListPanelSelectionChangedEvent.java
@@ -0,0 +1,26 @@
+package seedu.address.commons.events.ui;
+
+import seedu.address.commons.events.BaseEvent;
+import seedu.address.model.tasklist.TaskList;
+
+/**
+ * Represents a selection change in the Task List Panel
+ */
+public class ListPanelSelectionChangedEvent extends BaseEvent {
+
+
+    private final TaskList newSelection;
+
+    public ListPanelSelectionChangedEvent(TaskList newSelection) {
+        this.newSelection = newSelection;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+    public TaskList getNewSelection() {
+        return newSelection;
+    }
+}
