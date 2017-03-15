4308d9f962516041c936fbc1e60cc9594fe7f730
ZiyangMou <hflsmzy@gmail.com>
2017-03-15 21:52:57 +0800

    [Regex] Add regex for Date

--- src/main/java/seedu/address/model/task/Date.java
+++ src/main/java/seedu/address/model/task/Date.java
@@ -10,9 +10,18 @@ import seedu.address.commons.exceptions.IllegalValueException;
 public class Date implements Field {
 
     public static final String MESSAGE_DATE_CONSTRAINTS = "task due date should be the form dd/mm";
-    //TODO: public static final String DATE_VALIDATION_REGEX =
+    
+    public static final String DATE_VALIDATION_REGEX = ".*/.*";
+    public static final String DAY_MONTH_SEPARATOR = "/";
+    public static final String MONTH_VALIDATION_REGEX_1 = "([13578])|(0[13578])|(1[02])";
+    public static final String MONTH_VALIDATION_REGEX_2 = "([469])|(0[469])|(11)";
+    public static final String MONTH_VALIDATION_REGEX_3 = "(2)|(02)";
+    public static final String MONTH_VALIDATION_REGEX = MONTH_VALIDATION_REGEX_1 + "|" + MONTH_VALIDATION_REGEX_2 + "|" + MONTH_VALIDATION_REGEX_3;
+    public static final String DAY_VALIDATION_REGEX_1 = "([1-9])|(0[1-9])|(1\\d)|(2\\d)|(3[0-1])";
+    public static final String DAY_VALIDATION_REGEX_2 = "([1-9])|(0[1-9])|(1\\d)|(2\\d)|(30)";
+    public static final String DAY_VALIDATION_REGEX_3 = "([1-9])|(0[1-9])|(1\\d)|(2\\d)";
 
-    public final String value;
+    private final String value;
 
     /**
      * Validates given date.
@@ -32,11 +41,38 @@ public class Date implements Field {
      * Returns if a given string is a valid date.
      */
     public static boolean isValidDate(String test) {
-        return true;
-        //TODO: add REGEX
-        //return test.matches(EMAIL_VALIDATION_REGEX);
+        if (test.equals("")) {
+            return true;
+        }
+        if (!test.matches(DATE_VALIDATION_REGEX)) {
+            return false;
+        }
+        String[] dayAndMonth = test.split(DAY_MONTH_SEPARATOR);
+        String day = dayAndMonth[0];
+        String month = dayAndMonth[1];
+        return isValidMonth(month) && isValidDay(day, month);
+    }
+    
+    private static boolean isValidDay(String test, String month) {
+        if (month.matches(MONTH_VALIDATION_REGEX_1)) {
+            return test.matches(DAY_VALIDATION_REGEX_1);
+        } else if (month.matches(MONTH_VALIDATION_REGEX_2)) {
+            return test.matches(DAY_VALIDATION_REGEX_2);
+        } else if (month.matches(MONTH_VALIDATION_REGEX_3)) {
+            return test.matches(DAY_VALIDATION_REGEX_3);
+        }
+        assert false;
+        return false;
+    }
+    
+    private static boolean isValidMonth(String test) {
+        return test.matches(MONTH_VALIDATION_REGEX);
     }
 
+    public String getValue() {
+        return value;
+    }
+    
     @Override
     public String toString() {
         return value;
