d6ae6699180d80ba850499db63edda7699e2a733
ZiyangMou <hflsmzy@gmail.com>
2017-03-15 21:03:30 +0800

    [Regex] Add regex for time

--- src/main/java/seedu/address/model/task/Time.java
+++ src/main/java/seedu/address/model/task/Time.java
@@ -1,6 +1,5 @@
 package seedu.address.model.task;
 
-
 import seedu.address.commons.exceptions.IllegalValueException;
 
 /**
@@ -10,13 +9,16 @@ import seedu.address.commons.exceptions.IllegalValueException;
 public class Time implements Field {
 
     public static final String MESSAGE_TIME_CONSTRAINTS =
-            "Task time should be the form hh/mm";
+            "Task time should be the form hh:mm";
 
     /*
      * The first character of the time must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    // TODO: public static final String ADDRESS_VALIDATION_REGEX = "[^\\s].*";
+    public static final String HOUR_VALIDATION_REGEX = "(\\d)|(0\\d)|(1\\d)|(2[0-3])";
+    public static final String MINUTE_VALIDATION_REGEX = "[0-5][0-9]";
+    public static final String TIME_VALIDATION_REGEX = ".*:.*";
+    public static final String HOUR_MINUTE_SEPARATOR = ":";
 
     public final String value;
 
@@ -37,8 +39,24 @@ public class Time implements Field {
      * Returns true if a given string is a valid person email.
      */
     public static boolean isValidTime(String test) {
-        return true;
-        //TODO:return test.matches(ADDRESS_VALIDATION_REGEX);
+        if (test.equals("")) {
+            return true;
+        }
+        if (!test.matches(TIME_VALIDATION_REGEX)) {
+            return false;
+        }
+        String[] hourAndMinute = test.split(HOUR_MINUTE_SEPARATOR);
+        String hour = hourAndMinute[0];
+        String minute = hourAndMinute[1];
+        return isValidHour(hour) && isValidMinute(minute);
+    }
+
+    private static boolean isValidHour(String test) {
+        return test.matches(HOUR_VALIDATION_REGEX);
+    }
+
+    private static boolean isValidMinute(String test) {
+        return test.matches(MINUTE_VALIDATION_REGEX);
     }
 
     @Override
