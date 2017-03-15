7f98fb550ec45199ddd5ca68c8698ef58092b22d
ZiyangMou <hflsmzy@gmail.com>
2017-03-15 21:53:07 +0800

    [Test] Test Date regex

---
+++ src/test/java/seedu/address/model/task/DateTest.java
@@ -0,0 +1,36 @@
+//@@author A0147984L
+package seedu.address.model.task;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.model.task.Date;
+
+public class DateTest {
+
+    @Test
+    public void isValidDate() {
+        // invalid date
+        assertFalse(Date.isValidDate(" ")); // space only
+        assertFalse(Date.isValidDate("*")); // non-alphanumeric
+        assertFalse(Date.isValidDate("month")); // contains alphabets
+        assertFalse(Date.isValidDate("0/1")); // day less than 1
+        assertFalse(Date.isValidDate("1/0")); // month less than 1
+        assertFalse(Date.isValidDate("1/13")); // month more than 12
+        assertFalse(Date.isValidDate("32/1")); // day exceeds limit
+        assertFalse(Date.isValidDate("30/2")); // day exceeds limit
+        assertFalse(Date.isValidDate("31/4")); // day exceeds limit
+
+        // valid date
+        assertTrue(Date.isValidDate("")); // empty
+        assertTrue(Date.isValidDate("1/1")); // both day and month are 1 digit
+        assertTrue(Date.isValidDate("01/1")); // day is 2 digits
+        assertTrue(Date.isValidDate("01/01")); // month is 2 digits beginning with 0
+        assertTrue(Date.isValidDate("1/12")); // month is 2 digits beginning with 1
+        assertTrue(Date.isValidDate("31/1")); // day is 31 for January
+        assertTrue(Date.isValidDate("30/11")); // day is 30 for November
+        assertTrue(Date.isValidDate("29/2")); // day is 29 for February
+    }
+}
