ab278a060f3af728e4b7fad3dcab324d65fbd44b
ZiyangMou <hflsmzy@gmail.com>
2017-03-16 11:23:40 +0800

    [Test] Test Venue Regex

---
+++ src/test/java/seedu/address/model/task/VenueTest.java
@@ -0,0 +1,28 @@
+//@@author A0147984L
+package seedu.address.model.task;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.model.task.Venue;
+
+public class VenueTest {
+
+    @Test
+    public void isValidVenue() {
+        // invalid date
+        assertFalse(Venue.isValidVenue(" ")); // space only
+        assertFalse(Venue.isValidVenue(" NUS")); // begin with white space
+        assertFalse(Venue.isValidVenue("*NUS*")); // contains illegal punctuation
+
+        // valid date
+        assertTrue(Venue.isValidVenue("")); // empty
+        assertTrue(Venue.isValidVenue("NUS")); // alphabets only
+        assertTrue(Venue.isValidVenue("LT27")); // alphabets and digits
+        assertTrue(Venue.isValidVenue("Com2 bus stop")); // contains white spaces in between
+        assertTrue(Venue.isValidVenue("PGP #4-2")); // contains "#" and "-"
+        assertTrue(Venue.isValidVenue("Prince George's Park")); // contains "'"
+    }
+}
