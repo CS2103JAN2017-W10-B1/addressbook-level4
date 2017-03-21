c3ca30d3ee78fb5d703372b549f27093bfb7ce2a
Yan Xiaoxuan <E0012668@u.nus.edu>
2017-03-21 22:44:43 +0800

    [V0.3] Comment out unused imports

--- src/test/java/seedu/address/storage/XmlTaskManagerStorageTest.java
+++ src/test/java/seedu/address/storage/XmlTaskManagerStorageTest.java
@@ -1,6 +1,5 @@
 package seedu.address.storage;
 
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
@@ -11,7 +10,7 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
-import seedu.address.commons.exceptions.DataConversionException;
+//import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.commons.util.FileUtil;
 import seedu.address.model.ReadOnlyTaskManager;
 import seedu.address.model.TaskManager;
