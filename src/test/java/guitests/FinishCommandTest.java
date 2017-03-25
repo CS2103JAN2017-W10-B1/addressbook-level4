782ac412d7a53eba3b1da2b8e4ef11ebaab57b65
Yan Xiaoxuan <E0012668@u.nus.edu>
2017-03-25 19:40:17 +0800

    [V0.4][GUItest] Update FinishCommandTest

--- src/test/java/guitests/FinishCommandTest.java
+++ src/test/java/guitests/FinishCommandTest.java
@@ -1,36 +1,49 @@
 package guitests;
 
 import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.FinishCommand.MESSAGE_FINISH_TASK_SUCCESS;
+import static seedu.address.logic.commands.FinishCommand.MESSAGE_FINISH_TASK_MARKED;
 
 import org.junit.Test;
 
-import guitests.guihandles.TaskCardHandle;
 import seedu.address.testutil.TestTask;
 import seedu.address.testutil.TestUtil;
 
 public class FinishCommandTest extends TaskManagerGuiTest {
+    @Test
     public void finish() {
         //finish unfinished task
         TestTask[] currentList = td.getTypicalTasks();
-        TestTask taskToAdd = td.meeting;
-        assertFinishSuccess(taskToAdd, currentList);
-        currentList = TestUtil.addTasksToList(currentList, taskToAdd);
+        int targetIndex = 1;
+        assertFinishSuccess(targetIndex, currentList);
 
         //cannot finish finished task
+        currentList = td.getTypicalTasks();
+        targetIndex = 1;
+        commandBox.runCommand("finish " + targetIndex);
+        commandBox.runCommand("list all");
+        commandBox.runCommand("finish " + targetIndex);
+        assertResultMessage(MESSAGE_FINISH_TASK_MARKED);
         //invalid command, index must be positive integer and must not exceed current list length
         //invalid command, command word must be valid
         //invalid command,
     }
 
-    private void assertFinishSuccess(TestTask taskToAdd, TestTask... currentList) {
-        commandBox.runCommand(taskToAdd.getAddCommand());
+    /**
+     * Runs the finish command to finish the task at specified index and confirms the result is correct.
+     * @param targetIndexOneIndexed e.g. index 1 to finish the first task in the list,
+     * @param currentList A copy of the current unfinished list of tasks (before finish command).
+     */
+    private void assertFinishSuccess(int targetIndexOneIndexed, final TestTask[] currentList) {
+        TestTask taskToFinish = currentList[targetIndexOneIndexed - 1]; // -1 as array uses zero indexing
+        //TestTask[] expectedRemainder = TestUtil.removeTaskFromList(currentList, targetIndexOneIndexed);
+        TestTask[] expectedRemainder = currentList;
+        commandBox.runCommand("finish " + targetIndexOneIndexed);
 
-        //confirm the new card contains the right data
-        TaskCardHandle addedCard = taskListPanel.navigateToTask(taskToAdd);
-        assertMatching(taskToAdd, addedCard);
+        //confirm the list now contains all previous tasks except the finished task
+        assertTrue(taskListPanel.isListMatching(expectedRemainder));
 
-        //confirm the list now contains all previous persons plus the new person
-        TestTask[] expectedList = TestUtil.addTasksToList(currentList, taskToAdd);
-        assertTrue(taskListPanel.isListMatching(expectedList));
+        //confirm the result message is correct
+        assertResultMessage(String.format(MESSAGE_FINISH_TASK_SUCCESS, taskToFinish.toString()));
     }
 }
