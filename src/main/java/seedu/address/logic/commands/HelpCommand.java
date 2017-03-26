5ec61602d04656988a5bdd5497d42c9843a4233f
Yan Xiaoxuan <E0012668@u.nus.edu>
2017-03-26 15:38:22 +0800

    [V0.4] Remove unused imports

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -1,9 +1,5 @@
 package seedu.address.logic.commands;
 
-
-import seedu.address.commons.core.EventsCenter;
-import seedu.address.commons.events.ui.ShowHelpRequestEvent;
-
 /**
  * Format full help instructions for every command for display.
  */
