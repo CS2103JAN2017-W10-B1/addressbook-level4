17a688d2bf138852c63d7f955d5e2e4645b56dd1
Yan Xiaoxuan <E0012668@u.nus.edu>
2017-03-26 09:45:41 +0800

    [V0.4][ListCommand] Modify list command to identify non-existent list name

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -4,6 +4,10 @@ package seedu.address.logic.commands;
 import java.util.Iterator;
 import java.util.Set;
 
+import seedu.address.model.tag.UniqueTagList;
+import seedu.address.commons.core.UnmodifiableObservableList;
+import seedu.address.model.tag.Tag;
+
 /**
  * Lists all persons in the address book to the user.
  */
@@ -14,11 +18,12 @@ public class ListCommand extends Command {
     public static final String LIST_ALL = "all";
 
     public static final String MESSAGE_SUCCESS = "Listed all tasks";
+    public static final String MESSAGE_LIST_DOES_NOT_EXIST= "Given list name does not exist";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": List tasks as per the parameters\n"
             + "the specified listname/list index and displays them as a list with index numbers.\n"
             + "Parameters: [LISTNAME/LISTINDEX]\n"
-            + "Example: " + COMMAND_WORD + " CS2103";
+            + "Example: " + COMMAND_WORD + " study work";
 
     private static final String LIST_SEPARATOR = ", ";
 
@@ -37,6 +42,8 @@ public class ListCommand extends Command {
         if (keywords == null) {
             model.updateFilteredListToShowAllTasks();
             return new CommandResult(MESSAGE_SUCCESS);
+        } else if (!isKeywordMatchingTaglist()) {
+            return new CommandResult(MESSAGE_LIST_DOES_NOT_EXIST);
         } else if (keywords.contains(LIST_ALL)) {
             model.updateFilteredListToShowAllTasksAll();
             return new CommandResult(MESSAGE_SUCCESS);
@@ -45,6 +52,20 @@ public class ListCommand extends Command {
             return new CommandResult(formatter(keywords));
         }
     }
+
+//@@author A0147996E
+    private boolean isKeywordMatchingTaglist () {
+        boolean hasMatch = false;
+        UnmodifiableObservableList<Tag> tagList = UniqueTagList.asObservableList();
+        for(String keyword : keywords) {
+            for(Tag tag : tagList) {
+                if(tag.getName().equals(keyword)) {
+                    hasMatch = true;
+                }
+            }
+        } return hasMatch;
+    }
+//@@author
 
     private String formatter(Set<String> keywords) {
         String formatted = MESSAGE_SUCCESS + " in list ";
