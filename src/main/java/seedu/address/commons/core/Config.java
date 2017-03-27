456f86d5e73107a6ce18b10b039d647bf5f0e8dc
wangzexin <wang.zexin@dhs.sg>
2017-03-27 21:13:11 +0800

    [V0.4][Travis] Remove trailing whitespaces

--- src/main/java/seedu/address/commons/core/Config.java
+++ src/main/java/seedu/address/commons/core/Config.java
@@ -11,15 +11,12 @@ public class Config {
 
     public static final String DEFAULT_CONFIG_FILE = "config.json";
 
-    //@@author A0143409J
     // Config values customizable through config file
     private String appTitle = "Dueue App";
     private Level logLevel = Level.INFO;
     private String userPrefsFilePath = "preferences.json";
     private String dueueFilePath = "data/dueue.xml";
     private String dueueName = "MyTaskManager";
-    
-    //@@author A0143409J-reused
 
     public String getAppTitle() {
         return appTitle;
