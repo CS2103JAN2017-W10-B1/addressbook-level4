3e8000431481cd4cc8b1876241f405c961957fc3
wangzexin <wang.zexin@dhs.sg>
2017-03-27 21:22:49 +0800

    [V0.4][Test] Update listCommandParserTest

--- src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
@@ -12,7 +12,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.logic.commands.ListAllCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListFavoriteCommand;
+import seedu.address.logic.commands.ListFinishedCommand;
 
 public class ListCommandParserTest {
 
@@ -56,5 +59,92 @@ public class ListCommandParserTest {
         set.add("school");
 
         assertEquals(field.get(listCommand), set);
+    }
+
+    @Test
+    public void singleFieldFinishedTest() throws Exception {
+
+        Field field = ListFinishedCommand.class.getDeclaredField("keywords");
+        field.setAccessible(true);
+
+        ListFinishedCommand listFinishedCommand =  (ListFinishedCommand) ListCommandParser.parse("finished personal");
+
+        Set<String> set = new HashSet<String>();
+        set.add("personal");
+
+        assertEquals(field.get(listFinishedCommand), set);
+    }
+
+    @Test
+    public void multipleFieldFinishedTest() throws Exception {
+
+        Field field = ListFinishedCommand.class.getDeclaredField("keywords");
+        field.setAccessible(true);
+
+        ListFinishedCommand listFinishedCommand =  (ListFinishedCommand) ListCommandParser.parse("finished personal school");
+
+        Set<String> set = new HashSet<String>();
+        set.add("personal");
+        set.add("school");
+
+        assertEquals(field.get(listFinishedCommand), set);
+    }
+
+    @Test
+    public void singleFieldAllTest() throws Exception {
+
+        Field field = ListAllCommand.class.getDeclaredField("keywords");
+        field.setAccessible(true);
+
+        ListAllCommand listAllCommand =  (ListAllCommand) ListCommandParser.parse("all personal");
+
+        Set<String> set = new HashSet<String>();
+        set.add("personal");
+
+        assertEquals(field.get(listAllCommand), set);
+    }
+
+    @Test
+    public void multipleFieldAllTest() throws Exception {
+
+        Field field = ListAllCommand.class.getDeclaredField("keywords");
+        field.setAccessible(true);
+
+        ListAllCommand listAllCommand =  (ListAllCommand) ListCommandParser.parse("all personal school");
+
+        Set<String> set = new HashSet<String>();
+        set.add("personal");
+        set.add("school");
+
+        assertEquals(field.get(listAllCommand), set);
+    }
+
+    @Test
+    public void singleFieldFavoriteTest() throws Exception {
+
+        Field field = ListFavoriteCommand.class.getDeclaredField("keywords");
+        field.setAccessible(true);
+
+        ListFavoriteCommand listFavoriteCommand =  (ListFavoriteCommand) ListCommandParser.parse("favorite personal");
+
+        Set<String> set = new HashSet<String>();
+        set.add("personal");
+
+        assertEquals(field.get(listFavoriteCommand), set);
+    }
+
+    @Test
+    public void multipleFieldFavoriteTest() throws Exception {
+
+        Field field = ListFavoriteCommand.class.getDeclaredField("keywords");
+        field.setAccessible(true);
+
+        ListFavoriteCommand listFavoriteCommand =  (ListFavoriteCommand) ListCommandParser.parse("favorite personal school");
+
+        Set<String> set = new HashSet<String>();
+        set.add("personal");
+        set.add("school");
+
+        assertEquals(field.get(listFavoriteCommand), set);
     }
 }
