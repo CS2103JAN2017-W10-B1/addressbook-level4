6207daaba521bad30c719546b8b3fa27926c3a6f
Shermine Jong <shermine_jong_ruth_93@outlook.sg>
2017-03-28 18:24:21 +0800

    [V0.4][Logic] change finish task to be not undoable and stop undoing until a finish command is called

--- src/main/java/seedu/address/logic/commands/FinishCommand.java
+++ src/main/java/seedu/address/logic/commands/FinishCommand.java
@@ -16,14 +16,16 @@ import seedu.address.model.task.ReadOnlyTask.FinishProperty;
 import seedu.address.model.task.Task;
 import seedu.address.model.task.TaskDate;
 import seedu.address.model.task.TaskTime;
+import seedu.address.model.task.UniqueTaskList;
 import seedu.address.model.task.UniqueTaskList.DuplicateTaskException;
+import seedu.address.model.task.UniqueTaskList.TaskNotFoundException;
 import seedu.address.model.task.Venue;
 
 
 /**
  * Deletes a person identified using it's last displayed index from the address book.
  */
-public class FinishCommand extends AbleUndoCommand {
+public class FinishCommand extends Command {
 
     public static final String COMMAND_WORD = "finish";
 
@@ -102,37 +104,7 @@ public class FinishCommand extends AbleUndoCommand {
 
     @Override
     public boolean isUndoable() {
-        return true;
-    }
-
-    @Override
-    public CommandResult executeUndo(String message) throws CommandException {
-        return null;
-    }
-
-    @Override
-    public Command getUndoCommand() {
-        if (isSuccess) {
-            Task newTask = null;
-            if (task.isEvent()) {
-                try {
-                    newTask = new Event(task.getName(), ((Event) task).getStartDate(),
-                            ((Event) task).getStartTime(), task.getDate(), task.getTime(), task.getDescription(),
-                            task.getTag(), task.getVenue(), task.getPriority(), task.isFavorite(),
-                            FinishProperty.UNFINISHED);
-                } catch (IllegalValueException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            } else {
-                newTask = new Task(task.getName(), task.getDate(), task.getTime(), task.getDescription(),
-                        task.getTag(), task.getVenue(), task.getPriority(), task.isFavorite(),
-                        FinishProperty.UNFINISHED);
-            }
-            return new EditCommand(task, newTask);
-        } else {
-            return new IncorrectCommand(null);
-        }
+        return false;
     }
 }
 
