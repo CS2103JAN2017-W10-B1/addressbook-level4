72e80677ac49e936d9422a60465acd4ab94833af
wangzexin <wang.zexin@dhs.sg>
2017-03-28 23:17:27 +0800

    [V0.4][Test] Update test for event

--- src/test/java/seedu/address/model/task/EventTest.java
+++ src/test/java/seedu/address/model/task/EventTest.java
@@ -2,12 +2,14 @@
 package seedu.address.model.task;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.tag.Tag;
 import seedu.address.model.task.ReadOnlyTask.FinishProperty;
 
 public class EventTest {
@@ -77,37 +79,90 @@ public class EventTest {
         assertEquals(tester7.getTime().getValue(), "7:00");
         assertEquals(tester7.getStartTime().getValue(), "6:00");
         assertEquals(tester7.getFavoriteText(), "Favorite");
-        tester7.setFavorite(false);
-        assertEquals(tester7.getFavoriteText(), "");
 
         Event tester8 = new Event(tester7);
         assertEquals(tester8.getDate().getValue(), "20/12/2017");
         assertEquals(tester8.getStartDate().getValue(), "20/12/2017");
         assertEquals(tester8.getTime().getValue(), "7:00");
         assertEquals(tester8.getStartTime().getValue(), "6:00");
-        assertEquals(tester8.getFavoriteText(), "");
-        tester8.setFavorite(true);
         assertEquals(tester8.getFavoriteText(), "Favorite");
+        assertTrue(tester8.equals(tester7));
+    }
+    
+    @Test
+    public void resetTest() throws IllegalValueException {
+        Event tester1 = new Event(new Name("tester1"), new TaskDate("20/12/2017"), new TaskTime("6:00"),
+                new TaskDate("20/12/2017"), new TaskTime("7:00"), null, new Tag("abc"), null, null, false,
+                FinishProperty.UNFINISHED);
+        assertEquals(tester1.getDate().getValue(), "20/12/2017");
+        assertEquals(tester1.getStartDate().getValue(), "20/12/2017");
+        assertEquals(tester1.getTime().getValue(), "7:00");
+        assertEquals(tester1.getStartTime().getValue(), "6:00");
+
+        Event tester2 = new Event(new Name("tester2"), new TaskDate("30/12/2017"), new TaskTime("12:00"),
+                new TaskDate("30/12/2017"), new TaskTime("10:00"), null, new Tag("abc"), null, null, true,
+                FinishProperty.UNFINISHED);
+        assertEquals(tester2.getDate().getValue(), "30/12/2017");
+        assertEquals(tester2.getStartDate().getValue(), "30/12/2017");
+        assertEquals(tester2.getTime().getValue(), "10:00");
+        assertEquals(tester2.getStartTime().getValue(), "12:00");
+
+        tester2.resetData(tester1);
+        assertEquals(tester2.getDate().getValue(), "20/12/2017");
+        assertEquals(tester2.getStartDate().getValue(), "20/12/2017");
+        assertEquals(tester2.getTime().getValue(), "7:00");
+        assertEquals(tester2.getStartTime().getValue(), "6:00");
+    }
+
+    @Test
+    public void setterTest() throws IllegalValueException {
+        Event testEvent = new Event(new Name("tester7"), new TaskDate("20/12/2017"), new TaskTime("6:00"),
+                new TaskDate("20/12/2017"), new TaskTime("7:00"), null, null, null, null, true,
+                FinishProperty.UNFINISHED);
+        assertEquals(testEvent.getFavoriteText(), "Favorite");
+        testEvent.setDate(new TaskDate("30/12/2017"));
+        assertEquals(testEvent.getDate().toString(), "30/12/2017");
+        testEvent.setFavorite(false);
+        assertEquals(testEvent.getFavoriteText(), "");
+        testEvent.setFavorite(true);
+        assertEquals(testEvent.getFavoriteText(), "Favorite");
+        testEvent.setName(new Name("123"));
+        assertEquals(testEvent.getName().toString(), "123");
+        testEvent.setDescription(new Description("abc"));
+        assertEquals(testEvent.getDescription().toString(), "abc");
+        testEvent.setTag(new Tag("tager"));
+        assertEquals(testEvent.getTag().toString(), "[tager]");
+        testEvent.setVenue(new Venue("venue"));
+        assertEquals(testEvent.getVenue().toString(), "venue");
+        testEvent.setPriority(new Priority(null));
+        assertEquals(testEvent.getPriority().toString(), "2");
+        testEvent.setFinish(true);
+        assertEquals(testEvent.getFinished(), FinishProperty.FINISHED);
+        assertEquals(testEvent.getFinishedText(), "Finished");
+        testEvent.setStartDate(new TaskDate("30/12/2017"));
+        assertEquals(testEvent.getStartDate().toString(), "30/12/2017");
+        testEvent.setStartTime(new TaskTime("10:00"));
+        assertEquals(testEvent.getStartTime().toString(), "10:00");
     }
 
     @Test
     public void constructorInvalid1() throws IllegalValueException {
         thrown.expect(IllegalValueException.class);
-        Event tester = new Event(new Name("tester2"), new TaskDate("20/12/2017"), new TaskTime("6:00"),
+        new Event(new Name("tester2"), new TaskDate("20/12/2017"), new TaskTime("6:00"),
                 new TaskDate("19/12/2017"), new TaskTime("6:00"), null, null, null, null, false);
     }
 
     @Test
     public void constructorInvalid2() throws IllegalValueException {
         thrown.expect(IllegalValueException.class);
-        Event tester = new Event(new Name("tester3"), new TaskDate("20/12/2017"), new TaskTime("7:00"),
+        new Event(new Name("tester3"), new TaskDate("20/12/2017"), new TaskTime("7:00"),
                 new TaskDate("20/12/2017"), new TaskTime("6:00"), null, null, null, null, false);
     }
 
     @Test
     public void constructorInvalid3() throws IllegalValueException {
         thrown.expect(IllegalValueException.class);
-        Event tester = new Event(new Name("tester3"), new TaskDate("20/12/2017"), new TaskTime("7:00"),
+        new Event(new Name("tester3"), new TaskDate("20/12/2017"), new TaskTime("7:00"),
                 new TaskTime("6:00"), null, null, null, null, false);
     }
 }
