9c9a53996f60285a7bb9ad4abd726aadaf3c6e73
Shermine Jong <shermine_jong_ruth_93@outlook.sg>
2017-03-31 14:48:13 +0800

    [V0.5][Logic] edit clear command to support undo and redo

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -1,23 +1,29 @@
-//@@author generated
+//@@author A0138474X
 package seedu.address.logic.commands;
 
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.TaskManager;
 
 /**
  * Clears the address book.
  */
-public class ClearCommand extends Command {
+public class ClearCommand extends AbleUndoCommand {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Dueue has been cleared!";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Clear all tasks in Dueue \n"
             + "Parameters: Nil\n"
             + "Example: " + COMMAND_WORD;
+    
+    private TaskManager tasks;
 
 
     @Override
     public CommandResult execute() {
         assert model != null;
+        tasks = new TaskManager();
+        tasks.resetData(model.getTaskManager());
         model.resetData(new TaskManager());
         return new CommandResult(MESSAGE_SUCCESS);
     }
@@ -25,6 +31,23 @@ public class ClearCommand extends Command {
 
     @Override
     public boolean isUndoable() {
-        return false;
+        return true;
+    }
+
+
+    @Override
+    public CommandResult executeUndo(String message) throws CommandException {
+        assert model != null;
+        TaskManager tasks = new TaskManager();
+        tasks.resetData(model.getTaskManager());
+        model.resetData(this.tasks);
+        this.tasks = tasks;
+        return new CommandResult(message);
+    }
+
+
+    @Override
+    public Command getUndoCommand() throws IllegalValueException {
+        return this;
     }
 }
