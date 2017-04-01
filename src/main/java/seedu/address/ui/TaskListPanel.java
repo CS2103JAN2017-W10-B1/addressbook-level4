ca0b90005cd78406fe7ca5a56dd10f49c643a4e6
Yan Xiaoxuan <E0012668@u.nus.edu>
2017-04-01 10:27:44 +0800

    [V0.5rc][UI] scroll to mutated card upon command execution

--- src/main/java/seedu/address/ui/TaskListPanel.java
+++ src/main/java/seedu/address/ui/TaskListPanel.java
@@ -30,6 +30,7 @@ public class TaskListPanel extends UiPart<Region> {
     public TaskListPanel(AnchorPane taskListPlaceholder, ObservableList<ReadOnlyTask> taskList) {
         super(FXML);
         setConnections(taskList);
+        taskListView.scrollTo(0);
         addToPlaceholder(taskListPlaceholder);
     }
 
@@ -67,12 +68,11 @@ public class TaskListPanel extends UiPart<Region> {
         @Override
         protected void updateItem(ReadOnlyTask task, boolean empty) {
             super.updateItem(task, empty);
-
+            taskListView.scrollTo(getIndex() + 1);
             if (empty || task == null) {
                 setGraphic(null);
                 setText(null);
             } else {
-                taskListView.scrollTo(getIndex() + 1);
                 setGraphic(new TaskListCard(task, getIndex() + 1).getRoot());
             }
         }
