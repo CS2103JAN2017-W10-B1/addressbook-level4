0f95ae02bc0c091e029d7404e2709836d6984020
Shermine Jong <shermine_jong_ruth_93@outlook.sg>
2017-04-02 17:15:48 +0800

    [V0.5][Logic] edit add command parser to support redo of command

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -42,6 +42,7 @@ public class AddCommand extends AbleUndoCommand {
 
     private final Task toAdd;
     private boolean isSuccess;
+    private boolean isDeleteAllOcurrence = true;
 
     /**
      * Creates an AddCommand using raw values.
@@ -132,8 +133,9 @@ public class AddCommand extends AbleUndoCommand {
         }
     }
 
-    public AddCommand(ReadOnlyTask task) {
+    public AddCommand(ReadOnlyTask task, boolean isDeleteAllOcurrence) {
         this.toAdd = (Task) task;
+        this.isDeleteAllOcurrence = isDeleteAllOcurrence;
     }
 
     @Override
@@ -177,7 +179,7 @@ public class AddCommand extends AbleUndoCommand {
     @Override
     public Command getUndoCommand() {
         if (isSuccess) {
-            return new DeleteCommand(getTask());
+            return new DeleteCommand(getTask(), isDeleteAllOcurrence);
         } else {
             return new IncorrectCommand(null);
         }
