d1343c29b5fd8daa7b3c421f043cb897f7a6769d
Yan Xiaoxuan <E0012668@u.nus.edu>
2017-04-04 23:05:58 +0800

    [V0.5rc][GUItest]

--- src/test/java/guitests/ViewCommandTest.java
+++ src/test/java/guitests/ViewCommandTest.java
@@ -11,7 +11,16 @@ import seedu.address.testutil.TestTask;
 public class ViewCommandTest extends TaskManagerGuiTest {
 
     @Test
-    public void view_viewNextNDays_viewSuccess () {
+    public void view_viewNextNDays_viewSuccess() {
+        commandBox.runCommand(td.homework.getAddCommand());
+        commandBox.runCommand(td.homework2.getAddCommand());
+        assertViewResult("view next/1",  td.homework, td.homework2);
+        assertViewResult("view next/1000", td.homework, td.homework2, td.assignment,
+            td.gym, td.gym2, td.gym3, td.cs2103, td.date, td.study);
+    }
+
+    @Test
+    public void view_viewOnNDays_viewSuccess() {
         commandBox.runCommand(td.homework.getAddCommand());
         commandBox.runCommand(td.homework2.getAddCommand());
         assertViewResult("view next/1",  td.homework, td.homework2);
