252f2a3e9aca82510fc381542ee6a340b6907af0
wangzexin <wang.zexin@dhs.sg>
2017-04-04 22:43:11 +0800

    [V0.5RC][Logic] Update header comment in listCommands

--- src/main/java/seedu/address/logic/commands/ListAllCommand.java
+++ src/main/java/seedu/address/logic/commands/ListAllCommand.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Lists all persons in the address book to the user.
+ * Lists all tasks (finished and unfinished) in Dueue or in a specified list.
  */
 public class ListAllCommand extends Command {
 
