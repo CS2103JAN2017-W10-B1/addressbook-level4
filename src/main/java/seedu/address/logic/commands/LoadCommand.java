31d6bb130ad455f2fe04875423da30d072b240c0
Yan Xiaoxuan <E0012668@u.nus.edu>
2017-04-04 19:51:46 +0800

    [V0.5rc][Logic] Add loading file format exception

--- src/main/java/seedu/address/logic/commands/LoadCommand.java
+++ src/main/java/seedu/address/logic/commands/LoadCommand.java
@@ -13,16 +13,15 @@ import seedu.address.storage.StorageManager;
 public class LoadCommand extends AbleUndoCommand {
 
     public static final String COMMAND_WORD = "load";
-    public static final String MESSAGE_LOAD_SUCCESS =
-            "Successfully loaded tasks from directory: %1$s";
-    public static final String MESSAGE_LOAD_UNSUCCESS =
-            "Cannot load tasks from directory";
-    public static final String MESSAGE_FILE_NOT_FOUND = "Give directory is empty\n";
+    public static final String MESSAGE_ILLEGAL_DATA_TYPE = "File must be in XML format";
+    public static final String MESSAGE_LOAD_SUCCESS = "Successfully loaded tasks from directory: %1$s";
+    public static final String MESSAGE_LOAD_UNSUCCESS = "Cannot load tasks from directory";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Loads tasks from an external xml file\n"
             + "Parameters: Directory of the xml file\n"
             + "Example: " + COMMAND_WORD + "  /Users/Alice/Desktop/sampleData.xml";
     public static final String MESSAGE_DIRECTORY_NOT_GIVEN = "Directory not provided\n";
+    public static final String PATH_VALIDATION_REGEX = ".*xml";
 
     private String path;
     private TaskManager tempTaskManager;
@@ -39,19 +38,20 @@ public class LoadCommand extends AbleUndoCommand {
         assert model != null;
         tempTaskManager = TaskManager.getStub();
         tempTaskManager.resetData(model.getTaskManager());
-
+        if (!path.matches(PATH_VALIDATION_REGEX)) {
+            throw new CommandException(MESSAGE_ILLEGAL_DATA_TYPE);
+        }
         LOGGER.info(getClass() + " handles current command");
         try {
             assert path != null;
             StorageManager storage = new StorageManager(path);
             ReadOnlyTaskManager taskManager = storage.readTaskManager(path).get();
             model.resetData(taskManager);
-
             return new CommandResult(
                     CommandFormatter.undoFormatter(
                             String.format(MESSAGE_LOAD_SUCCESS, path), COMMAND_WORD));
         } catch (DataConversionException de) {
-            throw new CommandException(MESSAGE_LOAD_UNSUCCESS);
+            throw new CommandException(MESSAGE_ILLEGAL_DATA_TYPE);
         } catch (IOException e) {
             throw new CommandException(MESSAGE_LOAD_UNSUCCESS);
         }
