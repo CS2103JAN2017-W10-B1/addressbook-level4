22be9edd1fafc974f962f012f0b294a1dc2f3fb7
Yan Xiaoxuan <E0012668@u.nus.edu>
2017-04-04 21:08:36 +0800

    [V0.5rc][GUItest] Update several tests

--- src/test/java/guitests/UndoCommandTest.java
+++ src/test/java/guitests/UndoCommandTest.java
@@ -4,74 +4,72 @@ package guitests;
 import static org.junit.Assert.assertTrue;
 
 import static seedu.address.logic.commands.UndoCommand.MESSAGE_SUCCESS;
+import static seedu.address.logic.commands.UndoCommand.MESSAGE_UNSUCCESS;
 
 import org.junit.Test;
 
 import seedu.address.logic.commands.CommandFormatter;
 import seedu.address.testutil.TestEvent;
 import seedu.address.testutil.TestTask;
+import seedu.address.testutil.TestUtil;
 
 public class UndoCommandTest extends TaskManagerGuiTest {
+    private TestTask[] currentList = td.getTypicalTasks();
+    private String commandToUndo;
+    private String commandWord;
 
     @Test
-    public void undoAdd() {
-        TestTask[] currentList = td.getTypicalTasks();
-
+    public void undo_ableUndoCommands_undoSuccess() {
         TestEvent eventToAdd = te.assignment;
-        commandBox.runCommand(eventToAdd.getAddCommand());
-        assertUndoSuccess(currentList, "add");
+        commandToUndo = eventToAdd.getAddCommand();
+        commandWord = "add";
+        assertUndoSuccess(currentList, commandToUndo, commandWord);
 
-        TestTask taskToAdd = td.travel;
-        commandBox.runCommand(taskToAdd.getAddCommand());
-        assertUndoSuccess(currentList, "add");
-    }
+        commandToUndo = "delete 1";
+        commandWord = "delete";
+        assertUndoSuccess(currentList, commandToUndo, commandWord);
 
-    @Test
-    public void undoDelete() {
-        TestTask[] currentList = td.getTypicalTasks();
+        commandToUndo = "finish 1";
+        commandWord = "finish";
+        assertUndoSuccess(currentList, commandToUndo, commandWord);
 
-        int targetIndex = 2;
-        commandBox.runCommand("delete " + targetIndex);
-        assertUndoSuccess(currentList, "delete");
+        commandToUndo = "load data/dueue.xml";
+        commandWord = "load";
+        assertUndoSuccess(currentList, commandToUndo, commandWord);
 
-        targetIndex = 3;
-        commandBox.runCommand("delete " + targetIndex);
-        assertUndoSuccess(currentList, "delete");
-    }
-
-    @Test
-    public void undoFinish() {
-        TestTask[] currentList = td.getTypicalTasks();
-
-        int targetIndex = 2;
-        commandBox.runCommand("finish " + targetIndex);
-        assertUndoSuccess(currentList, "finish");
-
-        targetIndex = 3;
-        commandBox.runCommand("finish " + targetIndex);
-        assertUndoSuccess(currentList, "finish");
-    }
-
-    @Test
-    public void undoEdit() {
-        TestTask[] currentList = td.getTypicalTasks();
-
-        int targetIndex = 2;
         String detailsToEdit = " n/assignment2 dueT/18:30";
-        commandBox.runCommand("edit " + targetIndex + detailsToEdit);
-        assertUndoSuccess(currentList, "edit");
-
-        targetIndex = 4;
-        detailsToEdit = " start/03/05/ due/08/05 *u p/important #school";
-        commandBox.runCommand("edit " + targetIndex + detailsToEdit);
-        assertUndoSuccess(currentList, "edit");
+        commandToUndo = "edit 2 " + detailsToEdit;
+        commandWord = "edit";
+        assertUndoSuccess(currentList, commandToUndo, commandWord);
+    }
+    @Test
+    public void undo_noMoreCommandToUndo_undoUnsuccess() {
+        commandBox.runCommand("list all"); //list is not ableUndoCommand
+        commandBox.runCommand("undo");
+        assertResultMessage(MESSAGE_UNSUCCESS);
     }
 
-    private void assertUndoSuccess(final TestTask[] currentList, String commandWord) {
+    @Test
+    public void undo_stackOfAbleUndoCommands_undoSuccess() {
+        commandBox.runCommand("finish 1");
+        TestTask finishedTask = currentList[0];
+        currentList = TestUtil.removeTaskFromList(currentList, 1);
+
+        assertUndoSuccess(currentList, "clear", "clear");
+        currentList = TestUtil.addTasksToList(currentList, finishedTask);
+
         commandBox.runCommand("undo");
-        //confirm the list now contains all previous tasks
         assertTrue(taskListPanel.isListMatching(currentList));
-        //confirm the result message is correct
-        assertResultMessage(CommandFormatter.undoMessageFormatter(MESSAGE_SUCCESS, (commandWord + " command")));
+        assertResultMessage(CommandFormatter.undoMessageFormatter(MESSAGE_SUCCESS,
+                (commandWord + " command")));
+    }
+
+    private void assertUndoSuccess(final TestTask[] currentList,
+            String commandToUndo, String commandWord) {
+        commandBox.runCommand(commandToUndo);
+        commandBox.runCommand("undo");
+        assertTrue(taskListPanel.isListMatching(currentList));
+        assertResultMessage(CommandFormatter.undoMessageFormatter(MESSAGE_SUCCESS,
+                (commandWord + " command")));
     }
 }
